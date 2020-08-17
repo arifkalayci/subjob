fe9c283efc2c1202169deacd7054fb7adf56748c
Arif Kalayci <arifkalayci@gmail.com>
2020-08-18 00:04:27 +0300

    Can add and remove jobs

--- models/Runner.js
+++ models/Runner.js
@@ -18,11 +18,53 @@ class Runner {
     return this._api.isReady;
   }
 
+  async _onNewHead(blockHash) {
+    this.jobs.forEach(async job => {
+      if (!job.locked) {
+        job.locked = true;
+
+        await job.run(blockHash);
+
+        job.runCount++;
+        job.locked = false;
+      }
+    });
+  }
+
+  async _subHeads() {
+    const api = await this.api();
+    this.unsub = await api.rpc.chain.subscribeFinalizedHeads(header => this._onNewHead(header.hash));
+  }
+
   async runJob(plugin, blockHash, ...args) {
     const job = await plugin.newJob(this, ...args);
     job.run(blockHash);
 
     return job;
+  }
+
+  async addJob(plugin, ...args) {
+    const job = await plugin.newJob(this, ...args);
+
+    job.runCount = 0;
+    this.jobs.push(job);
+
+    if (this.jobs.length === 1) {
+      this._subHeads();
+    }
+
+    return job;
+  }
+
+  removeJob(job) {
+    const index = this.jobs.indexOf(job);
+    if (index > -1) {
+      this.jobs.splice(index, 1);
+    }
+
+    if (this.jobs.length === 0 && typeof(this.unsub) === 'function') {
+      this.unsub();
+    }
   }
 }
 
