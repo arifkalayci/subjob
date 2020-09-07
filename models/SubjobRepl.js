e2948690a19250ddf073002af3f258d192ec4357
Arif Kalayci <arifkalayci@gmail.com>
2020-09-07 18:43:27 +0300

    Refactor into 'SubjobRepl' model

---
+++ models/SubjobRepl.js
@@ -0,0 +1,87 @@
+const Repl = require("repl");
+const colors = require('colors');
+const path = require('path');
+const os = require('os');
+
+const Jobs = require('./Jobs');
+
+const Runners = require('./Runners');
+const Plugins = require('./Plugins');
+const Channels = require('./Channels');
+const Accounts = require('./Accounts');
+
+const HISTORY_FILE_NAME = '.subjob_history';
+
+class SubjobRepl {
+  constructor(socket) {
+    this._socket = socket;
+    global.jobs = new Jobs();
+  }
+
+  subjobContext(repl) {
+    const accounts = new Accounts();
+    const accountsVars = accounts.load();
+
+    const plugins = new Plugins();
+    const pluginsVars = plugins.load();
+
+    const channels = new Channels();
+    const channelsVars = channels.load();
+
+    const runners = new Runners(channels, this._socket);
+    const runnersVars = runners.load();
+
+    return {
+      listJobs: global.jobs.list.bind(global.jobs, this._repl.context.console),
+      removeJob: global.jobs.remove.bind(global.jobs),
+      clearJobs: global.jobs.clear.bind(global.jobs),
+      accounts,
+      ...accountsVars,
+      plugins,
+      ...pluginsVars,
+      channels,
+      ...channelsVars,
+      runners,
+      ...runnersVars
+    };
+  }
+
+  start() {
+    this._repl = Repl.start({
+      prompt: '>> '.brightBlue,
+      input: this._socket,
+      output: this._socket,
+      terminal: true,
+      preview: false,
+      historySize: 1000
+    });
+
+    this._repl.setupHistory(path.join(os.homedir(), HISTORY_FILE_NAME), err => {
+      if (err) {
+        logger.error(err);
+      }
+    });
+
+    Object.assign(this._repl.context, this.subjobContext());
+
+    this._repl.defineCommand('reload', {
+      help: 'Reload the context with all accounts, channels, plugins and runners',
+      action() {
+        this._repl.clearBufferedCommand();
+        this._socket.write(`Reloading context...\n`);
+
+        this._repl.resetContext();
+        Object.assign(this._repl.context, this.subjobContext());
+
+        this._repl.displayPrompt();
+      }
+    });
+
+    this._repl.on("exit", () => {
+      logger.info("Client disconnected");
+      this._socket.end();
+    });
+  }
+}
+
+module.exports = SubjobRepl;
--- server.js
+++ server.js
@@ -1,23 +1,11 @@
 const net = require("net");
-const Repl = require("repl");
-
-const Jobs = require('./models/Jobs');
-const Runners = require('./models/Runners');
-const Plugins = require('./models/Plugins');
-const Channels = require('./models/Channels');
-const Accounts = require('./models/Accounts');
-
-const path = require('path');
-const os = require('os');
-const HISTORY_FILE_NAME = '.subjob_history';
-
-const { LOG_DIR_NAME, ACCOUNTS_DIR_NAME, CHANNELS_DIR_NAME, PLUGINS_DIR_NAME, RUNNERS_DIR_NAME } = require('./constants');
-
-const fs = require('fs');
 
 const winston = require('winston');
 
-const colors = require('colors');
+const { LOG_DIR_NAME, ACCOUNTS_DIR_NAME, CHANNELS_DIR_NAME, PLUGINS_DIR_NAME, RUNNERS_DIR_NAME } = require('./constants');
+const fs = require('fs');
+
+const SubjobRepl = require('./models/SubjobRepl');
 
 [LOG_DIR_NAME, ACCOUNTS_DIR_NAME, CHANNELS_DIR_NAME, PLUGINS_DIR_NAME, RUNNERS_DIR_NAME].forEach(dirName => {
   if (!fs.existsSync(dirName)) {
@@ -37,74 +25,11 @@ global.logger = winston.createLogger({
   ]
 });
 
-global.jobs = new Jobs();
-
 const server = net.createServer(socket => {
-  const repl = Repl.start({
-    prompt: '>> '.brightBlue,
-    input: socket,
-    output: socket,
-    terminal: true,
-    preview: false,
-    historySize: 1000
-  });
-
-  repl.setupHistory(path.join(os.homedir(), HISTORY_FILE_NAME), err => {
-    if (err) {
-      logger.error(err);
-    }
-  });
-
-  function subjobContext() {
-    const accounts = new Accounts();
-    const accountsVars = accounts.load();
-
-    const plugins = new Plugins();
-    const pluginsVars = plugins.load();
-
-    const channels = new Channels();
-    const channelsVars = channels.load();
-
-    const runners = new Runners(channels, socket);
-    const runnersVars = runners.load();
-
-    return {
-      listJobs: global.jobs.list.bind(global.jobs, repl.context.console),
-      removeJob: global.jobs.remove.bind(global.jobs),
-      clearJobs: global.jobs.clear.bind(global.jobs),
-      accounts,
-      ...accountsVars,
-      plugins,
-      ...pluginsVars,
-      channels,
-      ...channelsVars,
-      runners,
-      ...runnersVars
-    };
-  }
-
-  repl.defineCommand('reload', {
-    help: 'Reload the context with all accounts, channels, plugins and runners',
-    action() {
-      this.clearBufferedCommand();
-      socket.write(`Reloading context...\n`);
-
-      this.resetContext();
-      Object.assign(this.context, subjobContext());
-
-      this.displayPrompt();
-    }
-  });
-
-  Object.assign(repl.context, subjobContext());
+  new SubjobRepl(socket).start();
 
   socket.on("error", e => {
     logger.error(`Socket error: ${e}`);
-  });
-
-  repl.on("exit", () => {
-    logger.info("Client disconnected");
-    socket.end();
   });
 });
 
